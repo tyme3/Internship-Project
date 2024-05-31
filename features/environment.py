2e88da8be08b6b542f273a4afb4e25ac68257035
Dimitri K <Demias2@gmail.com>
2024-05-30 17:52:46 -0700

    Added By and wait selenium commands

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from App.application import Application
 
 
